2496a70087b68216db95e245b75f8c947feae2d0
thearturca <arti.1046@gmail.com>
2024-08-22 12:50:48 +0300

    Initial commit

---
+++ prettier/index.mjs
@@ -0,0 +1,39 @@
+import prettierPluginPackageJson from "prettier-plugin-packagejson";
+
+/**
+ * Explicitly define config options that can be overriden by an .editorconfig
+ * Ref: https://github.com/prettier/prettier/blob/main/docs/configuration.md#editorconfig
+ *
+ * @type {import("prettier").Config}
+ */
+const config = {
+    endOfLine: "lf",
+    trailingComma: "all",
+    arrowParens: "avoid",
+    semi: true,
+    useTabs: false,
+    singleQuote: false,
+    tabWidth: 4,
+    printWidth: 80,
+    plugins: [prettierPluginPackageJson, "@ianvs/prettier-plugin-sort-imports"],
+    importOrder: [
+        "<BUILTIN_MODULES>",
+        "^(react|next(.*))$",
+        "<THIRD_PARTY_MODULES>",
+        "<TYPES>",
+        "",
+        "^@/(.*)$",
+        "^~/(.*)$",
+        "^[./]",
+        "<TYPES>^[.]",
+        "^(.*)(sass|css|scss)$",
+    ],
+    importOrderParserPlugins: [
+        "classProperties",
+        "decorators-legacy",
+        "jsx",
+        "typescript",
+    ],
+};
+
+export default config;
